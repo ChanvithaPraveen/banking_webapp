396308becb2967d071acb79408388587419e3399
Chanvitha Praveen <chanvithapraween@gmail.com>
2023-10-14 00:19:06 +0530

    initialize Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,39 @@
+// Login.js (React example)
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Send a POST request with username and password to your backend
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 200) {
+      // Redirect to the account balances page
+      window.location.href = '/account-balances';
+    } else {
+      // Handle login error
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
